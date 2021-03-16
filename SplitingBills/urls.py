c081e238f7c79711fd24e1b8eab13fabd9c5e8d1
kathmandu <mahhakatomanato@gmail.com>
2021-03-16 17:42:04 +0900

    add: routing
    大まかな画面遷移のルーティングを作成

--- SplitingBills/urls.py
+++ SplitingBills/urls.py
@@ -3,5 +3,21 @@ from . import views
 
 app_name = 'SplitingBills'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('about/', undefined, name='about'),  # アプリ概要
+
+    """割り勘(ログイン状態かどうかでUIを変える必要あり)"""
+    # 食材入力
+    path('spliting-bills-food', undefined, name='spliting_bills_food'),
+    # 誰と食事をしたかを入力
+    path('spliting-bills-who', undefined, name='spliting_bills_who'),
+    # 1人あたりの支払い金額
+    path('spliting-bills-result/', undefined, name='spliting_bills_result'),
+
+    """ユーザーがログインしているときのみ"""
+    path('history/', undefined, name='history'),  # 今までに作った料理の記録
+    # 貸し一覧(他人にお金を払ってもらっていない状態)
+    path('loan-list/', undefined, name='loan_list'),
+    # 借り一覧(他人にお金を払ってない状態)
+    path('borrowed-list/', undefined, name='borrowed_list'),
 ]
