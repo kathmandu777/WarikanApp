e1072bdef7e6cc3bb7338e2683c2c36231647f3a
kathmandu <mahhakatomanato@gmail.com>
2021-03-17 21:32:42 +0900

    add: money model base

--- SplitingBills/models.py
+++ SplitingBills/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+こんな感じで作っていく
+class Money(models.Model):
+    lender = models.ForeignKey(CustomUser, args="something")  # 貸した人
+    borrower = models.ForeignKey(CustomUser, args="something")  # 借りた人
+    amount = models.IntegerField()  # いくら
+"""
