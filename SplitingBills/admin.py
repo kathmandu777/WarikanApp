69d6e4203c14fc27a8bcf1442bac3ff045dee76e
rata <rtasansax@gmail.com>
2021-03-18 23:16:00 +0900

    add: admin
    管理サイトにモデルが表示されるように

--- SplitingBills/admin.py
+++ SplitingBills/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Food, Day, Person
 # Register your models here.
+
+admin.site.register(Food)
+admin.site.register(Day)
+admin.site.register(Person)
