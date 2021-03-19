1839ef42a3301e4c182960cf6827fb18445e5009
rata <rtasansax@gmail.com>
2021-03-19 23:06:44 +0900

    add: ResultView
    汎用ビュー

--- SplitingBills/views.py
+++ SplitingBills/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import Money
 
 # Create your views here.
 from django.http import HttpResponse
 
 
+class ResultView(TemplateView):
+    template_name = 'result.html'
+
+    def get_context_data(self, **kwargs):
+        # contextをtemplateに渡す
+        # memo: contextに、ページ上で表示したいデータを渡せば良い
+        
+        context = super().get_context_data(**kwargs)
+        
+        context['from_user'] = ['rata', 'user1']
+        context['to_user'] = ['user2', 'user3'] 
+        context['price'] = ['250', '150']
+        return context
+
 def food(request):
+    # Mealの入力フォーム
     return HttpResponse('This is food page.')
 
 
 def who(request):
+    # 人の入力フォーム
+    # Moneyにも記録
     return HttpResponse('This is who page.')
-
-
-def result(request):
-    return HttpResponse('This is result page.')
-
 
 def home(request):
     return render(request, 'home.html')
