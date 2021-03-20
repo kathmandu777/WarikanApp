a44af4ba09d79036e1e5ea1ca79c9e796e8e8ada
kathmandu <mahhakatomanato@gmail.com>
2021-03-21 00:23:27 +0900

    update: models

--- SplitingBills/models.py
+++ SplitingBills/models.py
@@ -13,26 +13,30 @@ class Money(models.Model):
 
 class Meal(models.Model):
     when = models.DateField(null=True, verbose_name='日付')
-    cost = models.IntegerField(verbose_name='価格')
+    cost = models.PositiveSmallIntegerField(verbose_name='価格')
     participant = models.ManyToManyField(CustomUser, verbose_name='参加者(複数)')
-    meal_name = models.CharField(max_length=50, verbose_name='食事名')
-    # idは自動でつけられる
-    payer = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True, verbose_name='購入者', related_name='Meal_payer')
-    
+    meal_name = models.CharField(max_length=50, verbose_name='料理名')
+    payer = models.ForeignKey(CustomUser, on_delete=models.PROTECT,
+                              null=True, verbose_name='購入者', related_name='Meal_payer')
 
+    # 管理画面などで表示される文字列を定義
     def __str__(self):
-        return self.meal_name # 表示名変更
+        return self.meal_name  # 表示名変更
 
 
 class Money(models.Model):
-    meal = models.ForeignKey(Meal, on_delete=models.PROTECT,verbose_name='どの食事についてか')
+    meal = models.ForeignKey(
+        Meal, on_delete=models.PROTECT, verbose_name='どの食事についてか')
     amount = models.IntegerField(verbose_name='金額')
-    lender = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True, verbose_name='貸した人', related_name='Money_lender')
-    borrower = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True, verbose_name='借りた人', related_name='Money_borrower')  # 借りた人
+    lender = models.ForeignKey(CustomUser, on_delete=models.PROTECT,
+                               null=True, verbose_name='貸した人', related_name='Money_lender')
+    borrower = models.ForeignKey(CustomUser, on_delete=models.PROTECT,
+                                 null=True, verbose_name='借りた人', related_name='Money_borrower')  # 借りた人
     is_Done = models.BooleanField(verbose_name='支払われたかどうか')
-    
+
     def __str__(self):
         return str(self.meal)
+
 
 """ old ver
 class Day(models.Model):
