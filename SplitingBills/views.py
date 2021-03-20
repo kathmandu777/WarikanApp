436be31cca0e1e8752b3b5423842afd2c85da426
kathmandu <mahhakatomanato@gmail.com>
2021-03-21 00:24:32 +0900

    update: FoodFormView

--- SplitingBills/views.py
+++ SplitingBills/views.py
@@ -1,48 +1,61 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponse
+from django.forms import formset_factory
 from .forms import FoodForm
-
 from .models import Money
 
 # Create your views here.
-from django.http import HttpResponse
 
 
-class ResultView(TemplateView):
+class ResultView(generic.TemplateView):
     template_name = 'result.html'
 
     def get_context_data(self, **kwargs):
         # contextをtemplateに渡す
         # memo: contextに、ページ上で表示したいデータを渡せば良い
-        
+
         context = super().get_context_data(**kwargs)
-        
+
         context['from_user'] = ['rata', 'user1']
-        context['to_user'] = ['user2', 'user3'] 
+        context['to_user'] = ['user2', 'user3']
         context['user_name'] = ['user4', 'user5']
         context['amount'] = ['250', '150']
-        context['money_list'] = [{'user_name': 'user1', 'amount': '250'},{'user_name': 'user2', 'amount': '350'}]
+        context['money_list'] = [{'user_name': 'user1', 'amount': '250'}, {
+            'user_name': 'user2', 'amount': '350'}]
         return context
 
+
 def food(request):
-    # Mealの入力フォーム
+    FoodFormSet = formset_factory(FoodForm, extra=5)
     if request.method == 'POST':
-        form = FoodForm(request.POST)
-        """
-        if form.is_valid():
-            form.save()
-            return redirect('who')
-        """
+        formset = FoodFormSet(request.POST)
+        if formset.is_valid():
+            meal_cost = [0] * 5
+            for form in formset:
+                food_name = form.cleaned_data.get('food_name')  # 使用しない
+                food_cost = form.cleaned_data.get('food_cost')
+                isUsedNum = 0
+                for i in range(5):
+                    if form.cleaned_data.get("isUsed" + str(i + 1)):
+                        isUsedNum += 1
+                for i in range(5):
+                    if form.cleaned_data.get("isUsed" + str(i + 1)):
+                        meal_cost[i] += food_cost / isUsedNum
+            print(meal_cost)
+            # return reverse_lazy("SplitingBills:spliting_bills_who")
     else:
-        form = FoodForm()
-    
-    return render(request, 'food.html', {'form': form})
+        formset = FoodFormSet()
+    return render(request, 'food.html', {'formset': formset})
 
 
 def who(request):
     # 人の入力フォーム
     # Moneyにも記録
     return HttpResponse('This is who page.')
+
 
 def home(request):
     return render(request, 'home.html')
