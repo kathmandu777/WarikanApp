8e9926ff985ed63142a811de4e20d8ad1132049d
kathmandu <mahhakatomanato@gmail.com>
2021-03-21 00:23:04 +0900

    update: FoodForm

--- SplitingBills/forms.py
+++ SplitingBills/forms.py
@@ -1,18 +1,15 @@
 from django import forms
 from .models import Meal, Money
- 
- 
+
+
 class FoodForm(forms.Form):
-    test = forms.CharField()
+    food_name = forms.CharField(label="食材名")
+    food_cost = forms.IntegerField(label="金額")
+    isUsed1 = forms.BooleanField(label="使用したか", required=False)
+    isUsed2 = forms.BooleanField(label="使用したか", required=False)
+    isUsed3 = forms.BooleanField(label="使用したか", required=False)
+    isUsed4 = forms.BooleanField(label="使用したか", required=False)
+    isUsed5 = forms.BooleanField(label="使用したか", required=False)
+
     def save(self):
         pass
-"""
-class FoodForm(forms.Form):
-# ModelFormを継承
-    class Meta():
-        model = Day
-        # どのmodelを利用するかmodel = モデル名で定義
-        fields = ('__all__')
-        # 表示するフィールド、'__all__'とすると全て
-        
-"""
