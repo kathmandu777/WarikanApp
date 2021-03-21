1c568d4883bccf88f9b28194cf808fafa50914cc
plmwa <townstgak@gmail.com>
2021-03-22 00:14:34 +0900

    .gitignoreとレシートocr

--- SplitingBills/forms.py
+++ SplitingBills/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from .models import Meal, Money
+import os
+import io
+from django.core.validators import FileExtensionValidator
+# 絶対パス必ず
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"絶対パスでjsonのある場所を指定"
+from google.cloud import vision
 
 
 class FoodForm(forms.Form):
@@ -16,25 +22,85 @@ class FoodForm(forms.Form):
 
 
 class UploadReceiptForm(forms.Form):
-    receipt = forms.ImageField(label="レシート")
+    receipt = forms.ImageField(label="レシート", validators=[
+                               FileExtensionValidator(['png', 'jpg'])])
 
     def ocr_func(self):
-        receipt = self.cleaned_data['receipt']
-        print(receipt)
-        # TODO:OCRの処理（以下の形式は例。どんな形でも大丈夫）
-        return [{"name": "milk", "cost": 150}, {"name": "banana", "cost": 200}]
+        receipt = self.cleaned_data['receipt'].read()
+        client = vision.ImageAnnotatorClient()
+        image = vision.Image(content=receipt)
+        response = client.document_text_detection(image=image)
+
+        def get_sorted_lines(response):
+            document = response.full_text_annotation
+            bounds = []
+            for page in document.pages:
+                for block in page.blocks:
+                    for paragraph in block.paragraphs:
+                        for word in paragraph.words:
+                            for symbol in word.symbols:
+                                x = symbol.bounding_box.vertices[0].x
+                                y = symbol.bounding_box.vertices[0].y
+                                text = symbol.text
+                                bounds.append(
+                                    [x, y, text, symbol.bounding_box])
+            bounds.sort(key=lambda x: x[1])
+            old_y = -1
+            line = []
+            lines = []
+            threshold = 1
+            for bound in bounds:
+                x = bound[0]
+                y = bound[1]
+                if old_y == -1:
+                    old_y = y
+                elif old_y - threshold <= y <= old_y + threshold:
+                    old_y = y
+                else:
+                    old_y = -1
+                    line.sort(key=lambda x: x[0])
+                    lines.append(line)
+                    line = []
+                line.append(bound)
+            line.sort(key=lambda x: x[0])
+            lines.append(line)
+            return lines
+        lines = get_sorted_lines(response)
+        name_list = []
+        price_list = []
+        food_name = ""
+        food_price = ""
+        check_point = 0
+        for line in lines:
+            texts = [i[2] for i in line]
+
+            for i in range(len(texts)):
+
+                if texts[i] != chr(165) and check_point == 0:
+                    food_name += texts[i]
+                elif texts[i] == chr(165):
+                    check_point = 1
+                else:
+                    food_price += texts[i]
+            check_point = 0
+            name_list.append(food_name)
+            price_list.append(food_price)
+            food_name = ""
+            food_price = ""
+
+        name_list = [a for a in name_list if a != '']
+        price_list = [a for a in price_list if a != '']
+        print(name_list)
+        print(price_list)
+        return name_list
 
 
 class WhoForm(forms.Form):
 
     user_name = forms.CharField(required=False)
-    
+
     isJoin1 = forms.BooleanField()
     isJoin2 = forms.BooleanField()
     isJoin3 = forms.BooleanField()
     isJoin4 = forms.BooleanField()
     isJoin5 = forms.BooleanField()
- 
-   
- 
-    
